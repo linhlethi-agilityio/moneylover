8390a4e4e2ac6d8f7e3f30e1df16823c6be76c38
Linh Thi Le <linh.lethi@asnet.com.vn>
2024-06-18 17:37:16 +0700

    implement sidebar and nav link dashboard

--- example/src/ui/dashboard/cards.tsx
+++ example/src/ui/dashboard/cards.tsx
@@ -4,6 +4,7 @@ import {
   UserGroupIcon,
   InboxIcon,
 } from '@heroicons/react/24/outline';
+import { lusitana } from '@/ui/fonts';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -29,7 +30,10 @@ export function Card({
         {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
       </div>
-      <p className="truncate rounded-xl bg-white px-4 py-8 text-center text-2xl">
+      <p
+        className={`${lusitana.className}
+          truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
+      >
         {value}
       </p>
     </div>
