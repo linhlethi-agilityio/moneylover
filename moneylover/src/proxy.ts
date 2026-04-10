f01be072d03021ebb9dde09a17dadfedbffc8a80
Linh Le Thi <linh.lethi@asnet.com.vn>
2026-04-10 16:57:12 +0700

    Replace middleware to proxy

--- moneylover/src/proxy.ts
+++ moneylover/src/proxy.ts
@@ -2,7 +2,7 @@ import NextAuth from 'next-auth';
 
 import { authConfig } from '@/configs/auth.config';
 
-export default NextAuth(authConfig).auth;
+export const proxy = NextAuth(authConfig).auth;
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$|.*\\.ico$|.*\\.webp$).*)'],
