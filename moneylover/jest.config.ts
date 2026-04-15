59c23ffd6208f36ef7974cd40431c72301c20c72
Linh Le Thi <linh.lethi@asnet.com.vn>
2026-04-15 16:49:46 +0700

    Setup jest testing

---
+++ moneylover/jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+};
+
+export default createJestConfig(config);
